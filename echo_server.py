d8d0ad8da5318849647a14c3995a3151fae21022
Lucas Salmons <lucsalmons@gmail.com>
2020-10-02 20:01:03 -0700

    instantiate and bind socket

--- echo_server.py
+++ echo_server.py
@@ -8,20 +8,20 @@ def server(log_buffer=sys.stderr):
     address = ('127.0.0.1', 10000)
     # TODO: Replace the following line with your code which will instantiate
     #       a TCP socket with IPv4 Addressing, call the socket you make 'sock'
-    sock = None
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP)
     # TODO: You may find that if you repeatedly run the server script it fails,
     #       claiming that the port is already used.  You can set an option on
     #       your socket that will fix this problem. We DID NOT talk about this
     #       in class. Find the correct option by reading the very end of the
     #       socket library documentation:
     #       http://docs.python.org/3/library/socket.html#example
-
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     # log that we are building a server
     print("making a server on {0}:{1}".format(*address), file=log_buffer)
 
     # TODO: bind your new sock 'sock' to the address above and begin to listen
     #       for incoming connections
-
+    sock.bind(address)
     try:
         # the outer loop controls the creation of new connection sockets. The
         # server will handle each incoming connection one at a time.
@@ -48,16 +48,16 @@ def server(log_buffer=sys.stderr):
                     #       formatting
                     data = b''
                     print('received "{0}"'.format(data.decode('utf8')))
-                    
+
                     # TODO: Send the data you received back to the client, log
                     # the fact using the print statement here.  It will help in
                     # debugging problems.
                     print('sent "{0}"'.format(data.decode('utf8')))
-                    
+
                     # TODO: Check here to see whether you have received the end
                     # of the message. If you have, then break from the `while True`
                     # loop.
-                    # 
+                    #
                     # Figuring out whether or not you have received the end of the
                     # message is a trick we learned in the lesson: if you don't
                     # remember then ask your classmates or instructor for a clue.
